0732793e5ee7abf4be87daee3e0dab6e52bfdf90
Syartaa <syartapajaziti@gmail.com>
2024-09-23 15:20:13 +0200

    Meals flutter project

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/widget/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen(
+      {super.key,
+      required this.onToggleFavorite,
+      required this.availableMeals});
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (cyx) => MealsScreen(
+              title: category.title,
+              meals: filteredMeals,
+              onToggleFavorite: onToggleFavorite,
+            ))); //Navigator.push(context, route)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        //availableCategories.map((category)=>CategoryGridItem(category:category)).toList();
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () => _selectCategory(context, category),
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/details_screen.dart';
+import 'package:meals_app/widget/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen(
+      {super.key,
+      this.title,
+      required this.meals,
+      required this.onToggleFavorite});
+
+  final String? title;
+  final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => DetailsScreen(
+              meal: meal,
+              onToggleFavorite: onToggleFavorite,
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+              meal: meals[index],
+              onSelectedMeal: (meal) {
+                selectMeal(context, meal);
+              },
+            ));
+
+    if (meals.isEmpty) {
+      content = Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "OHH Nothing in here!",
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            "Try selecting a diffrent category",
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+        ],
+      ));
+    }
+
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title!),
+        ),
+        body: content);
+  }
+}
